241ed01d681fc430ce7a37c4315a7825f62e3573
cngkts <goktascan1@gmail.com>
2020-09-30 16:21:39 +0300

    Initial commit

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField] private BuildingTypeSo buildingType;
+    private Camera mainCamera;
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(buildingType.Prefab,GetMouseWorldPosition(),Quaternion.identity);
+        }
+        
+    }
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
---
+++ Assets/Scripts/BuildingTypeSo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "ScriptableObjects/BuildingType")]
+public class BuildingTypeSo : ScriptableObject
+{
+    public string nameString;
+    public Transform Prefab;
+}
